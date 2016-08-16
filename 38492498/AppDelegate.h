f6bdee81de59f0837d6320549d68a7ab3a51c681
Kaolin Fire <kaolin@erif.org>
2016-08-16 13:59:54 -0700

    Initial Commit

---
+++ 38492498/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  38492498
+//
+//  Created by kaolin fire on 8/16/16.
+//  Copyright © 2016 kaolin fire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 38492498/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  38492498
+//
+//  Created by kaolin fire on 8/16/16.
+//  Copyright © 2016 kaolin fire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
